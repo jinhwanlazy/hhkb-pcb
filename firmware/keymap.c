7e45c97766d0f952882839a2ec5c508d44fb35c9
Jinhwan Choi <jinhwanlazy@gmail.com>
2017-03-31 05:06:24 +0900

    minimal vim like fn layer
    I might able to mimic all vim behaviors with keymap layers,
    but I decided to keep it simple.
    However, the insert key is an exception. It is never used without
    ctrl or shift, but also it is frequently used as a super set of
    ctrl-c/v.

--- firmware/keymap.c
+++ firmware/keymap.c
@@ -3,45 +3,49 @@
 /*
  * HHKB Layout
  */
-const uint8_t PROEMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: Default layer
      * ,-----------------------------------------------------------.
      * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  \|  `|
      * |-----------------------------------------------------------|
      * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Bspc |
      * |-----------------------------------------------------------|
-     * |Ctrl  |  A|  S|  D|  F|  G|  H|  J|  K|  L|Fn3|  '|Return  |
+     * |Ctrl  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return  |
      * |-----------------------------------------------------------|
      * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Fn |
      * |-----------------------------------------------------------|
-     * |    |Gui |Alt |      Space             |    |Alt |Gui |    |
+     * |    |alt |lgui|      space             |    |gui |alt |    |
      * `-----------------------------------------------------------'
      */
     KEYMAP(
         ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS, GRV, \
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,      BSPC,\
         LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,       \
-        LSFT,     Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,      FN0, \
-             LALT,LGUI,               SPC,                RGUI,RALT),
+        FN2,      Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,FN3,       FN0, \
+             FN4, LGUI,              FN1,                RGUI,RALT),
 
-    /* 1: HHKB Fn layer
-     * ,-----------------------------------------------------------.
-     * |Pwr| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
-     * |-----------------------------------------------------------|
-     * |Caps |   |   |   |   |   |   |   |Psc|Slk|Pus|Up |   |     |
-     * |-----------------------------------------------------------|
-     * |      |VoD|VoU|Mut|Ejc|   |  *|  /|Hom|PgU|Lef|Rig|Enter   |
-     * |-----------------------------------------------------------|
-     * |        |   |   |   |   |   |  +|  -|End|PgD|Dow|      |   |
-     * |-----------------------------------------------------------|
-     * |    |    |    |                        |    |    |    |    |
-     * `-----------------------------------------------------------'
-     */
+    /* 1: Default fn layer */
     KEYMAP(
         PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL,  \
         CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS,UP,  TRNS,     TRNS, \
         TRNS,VOLD,VOLU,MUTE,EJCT,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,     PENT,      \
         TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,TRNS,     TRNS, \
+             TRNS,TRNS,               TRNS,               TRNS,TRNS),
+
+    /* 2: MOD-ESC to tilde */
+    KEYMAP(
+        GRV, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,      \
+        TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS, \
+             TRNS,TRNS,               TRNS,               TRNS,TRNS),
+
+    /* 3: Vim like fn layer */
+    KEYMAP(
+        TRNS,F1  ,F2  ,F3  ,F4  ,F5  ,F6  ,F7  ,F8  ,F9  ,F10 ,F11 ,F12, PSCR,SLCK, \
+        TRNS,TRNS,TRNS,END, TRNS,TRNS,FN5, PGUP,TRNS,TRNS,FN6, TRNS,TRNS,     TRNS, \
+        TRNS,TRNS,TRNS,PGDN,TRNS,TRNS,LEFT,DOWN,UP,  RGHT,TRNS,TRNS,     PENT,      \
+        TRNS,     TRNS,DEL, TRNS,TRNS,HOME,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS, \
              TRNS,TRNS,               TRNS,               TRNS,TRNS),
 };
 
@@ -50,4 +54,11 @@ const uint8_t PROEMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 const action_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1),
+    [1] = ACTION_LAYER_TAP_KEY(3, KC_SPACE),
+    [2] = ACTION_LAYER_MODS(2, MOD_LSFT),
+    [3] = ACTION_LAYER_MODS(2, MOD_RSFT),
+    [4] = ACTION_LAYER_MODS(2, MOD_LALT),
+
+    [5] = ACTION_MODS_KEY(MOD_LCTL, KC_INS),
+    [6] = ACTION_MODS_KEY(MOD_LSFT, KC_INS),
 };
