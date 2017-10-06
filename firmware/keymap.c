172ed6ff3bb998c5addb92f5a2f009792657f041
Jinhwan Choi <jinhwanlazy@gmail.com>
2017-10-06 16:47:41 +0900

    LCTRL now is a dual perpose key, works as additional ESC when tapped

--- firmware/keymap.c
+++ firmware/keymap.c
@@ -20,7 +20,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP(
         ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS, GRV, \
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,      BSPC,\
-        LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,       \
+        FN7, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,       \
         FN2,      Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,FN3,       FN0, \
              FN4, LGUI,              FN1,                RGUI,RALT),
 
@@ -86,4 +86,5 @@ const action_t PROGMEM fn_actions[] = {
 
     [5] = ACTION_MODS_KEY(MOD_LCTL, KC_INS),
     [6] = ACTION_MODS_KEY(MOD_LSFT, KC_INS),
+    [7] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),
 };
